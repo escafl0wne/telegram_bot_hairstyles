82f470463e44e9339d917df29b1133cfd8dbfcca
escafl0wne <falladvance@outlook.com>
2024-01-02 21:53:32 +0500

    sd

--- src/bot.class.ts
+++ src/bot.class.ts
@@ -34,7 +34,8 @@ export class Bot {
     }
     await this.bot.launch({
       webhook: {
-        domain: 'https://telegrambothairstyles.vercel.app',
+        domain: 'https://telegrambothairstyles.vercel.tld',
+
         port: 3000,
       },
     });
