72d827525bac0dab06fd1e8b1151da55da36fdce
escafl0wne <falladvance@outlook.com>
2024-01-02 19:42:02 +0500

    ff

--- src/bot.class.ts
+++ src/bot.class.ts
@@ -2,7 +2,7 @@ import { Telegraf } from 'telegraf';
 import { TBotContext } from './types';
 import { TConfigService } from './config/config.service';
 import LocalSession from 'telegraf-session-local';
-import { Command, Service } from './commands/commands.class';
+import { Command } from './commands/commands.class';
 import { StartCommand } from './commands/start.command';
 import { SelectionCommand } from './commands/selection.command';
 
@@ -33,15 +33,9 @@ export class Bot {
       command.handle();
     }
 
-    await this.bot
-      .launch({
-        webhook: {
-          domain: 'https://api.telegram.org/bot6723599492:AAFyakSDGl6ot1fMNOP-6cdHCELxtQtV9Rs/setWebhook',
-        },
-      })
-      .then(() => {
-        console.info(`The bot ${this.bot.botInfo.username} is running on server`);
-      });
+    await this.bot.launch().then(() => {
+      console.info(`The bot ${this.bot.botInfo.username} is running on server`);
+    });
 
     process.once('SIGINT', () => this.bot.stop('SIGINT'));
     process.once('SIGTERM', () => this.bot.stop('SIGTERM'));
